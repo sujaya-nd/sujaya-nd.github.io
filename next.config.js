df7cd355c145a31645b5432b50a32e4535fe8853
Sujay Alaspure <sujay.alaspure@netradyne.com>
2023-10-04 22:24:20 +0530

    Added Route

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: "export",
   distDir: "build",
+  images: {unoptimized: true},
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Sujay Alaspure</h1>
+      <Link href="/home">
+        <Image src="/favicon.ico" alt="logo" width="100" height="100" />
+      </Link>
     </main>
   )
 }
